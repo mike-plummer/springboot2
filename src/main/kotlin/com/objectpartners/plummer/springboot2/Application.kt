c688a73ffe586d536c7ad7b36fb60278a26360bd
Mike Plummer <mike-plummer@users.noreply.github.com>
2018-03-31 20:08:57 -0500

    feat(testing): Testing
    Adding test setup and initial unit test
    Enabling circleci

--- src/main/kotlin/com/objectpartners/plummer/springboot2/Application.kt
+++ src/main/kotlin/com/objectpartners/plummer/springboot2/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Springboot2Application
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<Springboot2Application>(*args)
+    runApplication<Application>(*args)
 }
